b66f6f8e04f983176363432b07bb216d19e0f888
nindidooo <matthewcsbrown@gmail.com>
2021-07-17 19:58:48 +1000

    initial working on mac version

--- run_pose_estimation.py
+++ run_pose_estimation.py
@@ -29,13 +29,13 @@ import importlib.util
 import datetime
 
 import time
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-#led
-GPIO.setup(4, GPIO.OUT)
-#button
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setmode(GPIO.BCM)
+# #led
+# GPIO.setup(4, GPIO.OUT)
+# #button
+# GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 # Define VideoStream class to handle streaming of video from webcam in separate processing thread
@@ -97,8 +97,7 @@ parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If t
                     default='1280x720')
 parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
                     action='store_true')
-parser.add_argument('--output_path', help="Where to save processed imges from pi.",
-                    required=True)
+parser.add_argument('--output_path', help="Where to save processed imges from pi.")
 
 args = parser.parse_args()
 
@@ -230,12 +229,13 @@ try:
     while True:
     #if True:
         #make sure LED is off and wait for button press
-        if not led_on and  not GPIO.input(17):
+        if not led_on: #and  not GPIO.input(17):
         #if True:
             #timestamp an output directory for each capture
-            outdir = pathlib.Path(args.output_path) / time.strftime('%Y-%m-%d_%H-%M-%S-%Z')
-            outdir.mkdir(parents=True)
-            GPIO.output(4, True)
+            if(args.output_path):
+                outdir = pathlib.Path(args.output_path) / time.strftime('%Y-%m-%d_%H-%M-%S-%Z')
+                outdir.mkdir(parents=True)
+            # GPIO.output(4, True)
             time.sleep(.1)
             led_on = True
             f = []
@@ -298,8 +298,8 @@ try:
                 frame_resized = draw_lines(keypoint_positions, frame_resized, drop_pts)
 
                 # Draw framerate in corner of frame - remove for small image display
-                #cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
-                #cv2.putText(frame_resized,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+                cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+                cv2.putText(frame_resized,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
 
                 # Calculate framerate
                 t2 = cv2.getTickCount()
@@ -308,14 +308,18 @@ try:
                 f.append(frame_rate_calc)
     
                 #save image with time stamp to directory
-                path = str(outdir) + '/'  + str(datetime.datetime.now()) + ".jpg"
+                if(args.output_path):
+                    path = str(outdir) + '/'  + str(datetime.datetime.now()) + ".jpg"
 
-                status = cv2.imwrite(path, frame_resized)
+                # status = cv2.imwrite(path, frame_resized)
+                cv2.imshow("Capturing",frame_resized)
+
 
                 # Press 'q' to quit
-                if cv2.waitKey(1) == ord('q') or led_on and not GPIO.input(17):
-                    print(f"Saved images to: {outdir}")
-                    GPIO.output(4, False)
+                if cv2.waitKey(1) == ord('q'): #or led_on and not GPIO.input(17):
+                    if(args.output_path):
+                        print(f"Saved images to: {outdir}")
+                    # GPIO.output(4, False)
                     led_on = False
                     # Clean up
                     cv2.destroyAllWindows()
@@ -328,6 +332,9 @@ except KeyboardInterrupt:
     cv2.destroyAllWindows()
     videostream.stop()
     print('Stopped video stream.')
-    GPIO.output(4, False)
-    GPIO.cleanup()
+    # GPIO.output(4, False)
+    # GPIO.cleanup()
     #print(str(sum(f)/len(f)))
+
+
+# python3 run_pose_estimation.py --modeldir posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite --output_path pose_images
